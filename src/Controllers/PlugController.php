b206fee199c45d5c25c3fd2bdaee1780aa955d0a
Yagnesh Chavda <con.boringmacho@gmail.com>
2023-08-11 18:13:57 +0530

    BDroppy v5

--- src/Controllers/PlugController.php
+++ src/Controllers/PlugController.php
@@ -2,21 +2,44 @@
 
 namespace Plug\Controllers;
 
+use Exception;
 use Plenty\Plugin\Controller;
 use Plenty\Plugin\Templates\Twig;
-use Plug\Services\Request;
+
 
 
 
 class PlugController extends Controller
 {
+
+    function fetchData($url)
+    {
+        try {
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $response = curl_exec($ch);
+            curl_close($ch);
+
+            if ($response === false) {
+                throw new Exception('Error fetching data from API');
+            }
+
+            return json_decode($response, true);
+        } catch (Exception $th) {
+            die($th);
+        }
+    }
+
+
     /**
      * @param Twig $twig
      * @return string
      */
-    public function getHelloWorldPage(Twig $twig):string
+    public function getHelloWorldPage(Twig $twig): string
     {
-        // $RequestInstance = new Request();
-        return $twig->render('Plug::Index');
+        $data = $this->fetchData('../../data/data.json');
+        return $twig->render('Plug::Index', [
+            'data' => $data,
+        ]);
     }
 }
